01c68a4e733e78df17e9d839e937a07f72135cce
Rinalds Pudulis <rpudulis@gmail.com>
2017-12-24 06:29:16 +0200

    feat: Setting up redux store

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import reducers from './reducers';
+
 import "./index.css";
 import Root from "./components/Root";
 import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(<Root />, document.getElementById("root"));
+const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
+const store = createStoreWithMiddleware(reducers);
+
+ReactDOM.render(<Provider store={store}><Root /></Provider>, document.getElementById("root"));
 registerServiceWorker();
