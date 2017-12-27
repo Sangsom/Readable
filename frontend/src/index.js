d42a3fbc5131590b98bae9e8a26e2d8952b28a0d
Rinalds Pudulis <rpudulis@gmail.com>
2017-12-27 09:53:54 +0200

    chore: Setting up redux devtools

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,24 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import { BrowserRouter as Router } from 'react-router-dom';
-import thunk from 'redux-thunk';
-import reducers from './reducers';
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, compose } from "redux";
+import { BrowserRouter as Router } from "react-router-dom";
+import thunk from "redux-thunk";
+import reducers from "./reducers";
 
 import "./index.css";
 import Root from "./components/Root";
 import registerServiceWorker from "./registerServiceWorker";
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
-const store = createStoreWithMiddleware(reducers);
+const store = createStoreWithMiddleware(reducers, composeEnhancers());
 
 ReactDOM.render(
-    <Provider store={store}>
-        <Router>
-            <Root />
-        </Router>
-    </Provider>,
- document.getElementById("root")
+  <Provider store={store}>
+    <Router>
+      <Root />
+    </Router>
+  </Provider>,
+  document.getElementById("root")
 );
 registerServiceWorker();
