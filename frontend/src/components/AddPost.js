fef62ce31c07bf0f0597c0570d6d88999f496afb
Rinalds Pudulis <rpudulis@gmail.com>
2018-01-03 13:50:21 +0200

    refactor: Added ID and Category field

--- frontend/src/components/AddPost.js
+++ frontend/src/components/AddPost.js
@@ -8,12 +8,12 @@ class AddPost extends Component {
     const { refs } = this;
 
     const new_post = {
-      id: "asxzazas",
+      id: refs.id.value,
       timestamp: Date.now(),
       title: refs.title.value,
       body: refs.body.value,
       author: refs.author.value,
-      category: "react"
+      category: refs.category.value
     };
 
     this.props.addPost(new_post);
@@ -23,6 +23,9 @@ class AddPost extends Component {
     return (
       <div>
         <form onSubmit={this.handleSubmit}>
+          <label>ID</label>
+          <input type="text" ref="id" name="id" />
+          <br />
           <label>Title</label>
           <input type="text" ref="title" name="title" />
           <br />
@@ -31,6 +34,9 @@ class AddPost extends Component {
           <br />
           <label htmlFor="">Author</label>
           <input type="text" ref="author" name="author" />
+          <br />
+          <label htmlFor="">Category</label>
+          <input type="text" ref="category" name="category" />
           <br />
           <input type="submit" value="Submit" />
           <br />
