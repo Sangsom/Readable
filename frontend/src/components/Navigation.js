42f3231a13e9bafd8988a89dcd3307567d628c7f
Rinalds Pudulis <rpudulis@gmail.com>
2018-01-08 16:12:49 +0200

    refactor: Recreated categories menu - Navigation component

--- frontend/src/components/Navigation.js
+++ frontend/src/components/Navigation.js
@@ -2,28 +2,46 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import fetchCategories from "../actions/fetch_categories";
+import { Header, Menu } from "semantic-ui-react";
 
 class Navigation extends Component {
+  state = {
+    activeItem: "all"
+  };
+
   componentDidMount() {
     this.props.fetchCategories();
   }
 
+  handleActiveItem = (e, { name }) => {
+    this.setState({ activeItem: name });
+  };
+
   render() {
+    const { activeItem } = this.state;
     const { categories } = this.props;
 
     return (
       <div>
-        Categories:
-        <ul>
-          <li>
-            <Link to="/">Show all</Link>
-          </li>
+        <Header size="large">Categories</Header>
+        <Menu vertical>
+          <Menu.Item
+            as={Link}
+            to="/"
+            name="all"
+            active={activeItem === "all"}
+          />
           {categories.map(category => (
-            <li key={category.name}>
-              <Link to={`/${category.path}`}>{category.name}</Link>
-            </li>
+            <Menu.Item
+              as={Link}
+              to={`/${category.path}`}
+              name={category.name}
+              key={category.name}
+              active={activeItem === category.name}
+              onClick={this.handleActiveItem}
+            />
           ))}
-        </ul>
+        </Menu>
         <Link to="/add-post">
           <button>Add Post</button>
         </Link>
