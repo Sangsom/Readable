43a52927e8a8ec7b669974aa3061dfc90e9ab042
Rinalds Pudulis <rpudulis@gmail.com>
2018-01-11 09:19:26 +0200

    refactor: Optimizing Add comments code - removing redirection

--- frontend/src/components/AddComment.js
+++ frontend/src/components/AddComment.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { Redirect } from "react-router-dom";
 import addComment from "../actions/add_comment";
 import { Button, Modal, Form } from "semantic-ui-react";
 
@@ -11,8 +10,7 @@ class AddComment extends Component {
     body: "",
     author: "",
     parentId: "",
-    modalOpen: false,
-    redirect: false
+    modalOpen: false
   };
 
   componentDidMount() {
@@ -30,7 +28,8 @@ class AddComment extends Component {
 
   handleChange = (e, { name, value }) => this.setState({ [name]: value });
 
-  handleSubmit = () => {
+  handleSubmit = e => {
+    e.preventDefault();
     const { body, author, parentId } = this.state;
     const new_comment = {
       id: uuidv1(),
@@ -41,57 +40,51 @@ class AddComment extends Component {
     };
 
     this.props.addComment(new_comment, () => {
-      this.setState({ redirect: true });
+      this.setState({ redirect: true, modalOpen: false });
     });
   };
 
   render() {
-    const { author, body, parentId } = this.state;
+    const { author, body } = this.state;
 
     return (
-      <div>
-        {this.state.redirect ? (
-          <Redirect to={`/posts/${parentId}`} />
-        ) : (
-          <Modal
-            dimmer="blurring"
-            trigger={
-              <Button onClick={this.handleOpen} positive>
-                Add new comment
+      <Modal
+        dimmer="blurring"
+        trigger={
+          <Button onClick={this.handleOpen} positive>
+            Add new comment
+          </Button>
+        }
+        open={this.state.modalOpen}
+        onClose={this.handleClose}
+      >
+        <Modal.Header>Post a comment to this post</Modal.Header>
+        <Modal.Content>
+          <Modal.Description>
+            <Form onSubmit={this.handleSubmit}>
+              <Form.Input
+                label="Author"
+                type="text"
+                placeholder="Author"
+                name="author"
+                value={author}
+                onChange={this.handleChange}
+              />
+              <Form.TextArea
+                label="Comment"
+                placeholder="Leave a comment here"
+                name="body"
+                value={body}
+                onChange={this.handleChange}
+              />
+              <Button onClick={this.handleClose}>Cancel</Button>
+              <Button positive type="submit">
+                Submit
               </Button>
-            }
-            open={this.state.modalOpen}
-            onClose={this.handleClose}
-          >
-            <Modal.Header>Post a comment to this post</Modal.Header>
-            <Modal.Content>
-              <Modal.Description>
-                <Form onSubmit={this.handleSubmit}>
-                  <Form.Input
-                    label="Author"
-                    type="text"
-                    placeholder="Author"
-                    name="author"
-                    value={author}
-                    onChange={this.handleChange}
-                  />
-                  <Form.TextArea
-                    label="Comment"
-                    placeholder="Leave a comment here"
-                    name="body"
-                    value={body}
-                    onChange={this.handleChange}
-                  />
-                  <Button onClick={this.handleModalClose}>Cancel</Button>
-                  <Button positive type="submit">
-                    Submit
-                  </Button>
-                </Form>
-              </Modal.Description>
-            </Modal.Content>
-          </Modal>
-        )}
-      </div>
+            </Form>
+          </Modal.Description>
+        </Modal.Content>
+      </Modal>
     );
   }
 }
